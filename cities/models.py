a94b3dbe12565df5586c70da9bd06dff0dd676cb
Ellie Farrer <elliefarrermusic@gmail.com>
2018-11-01 01:14:22 +0000

    Added basic db

--- cities/models.py
+++ cities/models.py
@@ -4,3 +4,5 @@ class City(models.Model):
     name = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     population = models.IntegerField()
+    def __str__(self):
+        return self.name 
