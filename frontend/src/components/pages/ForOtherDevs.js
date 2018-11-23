fefbbfae3aae554e82ae41d1550f380287b06702
Ellie Farrer <elliefarrermusic@gmail.com>
2018-11-23 10:28:19 +0000

    Added for other devs page

--- frontend/src/components/pages/ForOtherDevs.js
+++ frontend/src/components/pages/ForOtherDevs.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default class ForOtherDevs extends React.Component {
   render() {
     return (
-      <h2>About the Build</h2>
+      <section>
+        <h2>About the Build</h2>
+
+        <p>Vacilando was my first go at building a "proper" project in Python (Python 3.7, that is), my first ever go at doing anything with Django, and a welcome revisit to ReactJS. My React front end is built with Webpack, and the site is styled with SCSS and Bootstrap. I manage all my JS files with Homebrew.</p>
+
+        <p>View the repo <a href="https://github.com/platypotomus/python-react-travel-app">here</a></p>
+
+        <p>License: MIT</p>
+      </section>
     )
   }
 }
